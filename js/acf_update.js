f10251d15bb40684f358c2e2ed5511481d7fe03d
Colton <allen.colton@hotmail.com>
2022-05-25 11:44:31 -0700

    updated vanilla js

--- js/acf_update.js
+++ js/acf_update.js
@@ -1,21 +1,19 @@
 window.addEventListener('DOMContentLoaded', function() {
-	document.querySelector('.acf-table-root')?.querySelector('.acf-table-wrap')?.prepend('<div>To indicate a service description, prepend the text with "++".<br />For example: ++This is a service description.<br /><br /></div>');	
-
-
-	document.querySelectorAll('.acf-table-body-row')?.each(function(){
-		$leftbody = document.querySelector(this).querySelector('.acf-table-body-left');
-		$count = document.querySelector(this).querySelector('.acf-table-body-cont').html();
-		document.querySelector( $leftbody ).insertAdjacentHTML("beforeend", '<div class="is-description"><input type="checkbox" value="description" id="description' + $count + '" name="description" /><label for="description' + $count + '"></label></div>' );
-		console.log($count);
+	document.querySelector('.acf-table-root')?.querySelectorAll('.acf-table-wrap')?.prepend('<div>To indicate a service description, prepend the text with "++".<br />For example: ++This is a service description.<br /><br /></div>');
+	
+	document.querySelectorAll('.acf-table-body-row')?.forEach(el => {
+		const leftBody = el.querySelectorAll('.acf-table-body-left');
+		const count = el.querySelectorAll('.acf-table-body-cont').innerHTML;
+		leftBody.appendChild('<div class="is-description"><input type="checkbox" value="description" id="description' + count + '" name="description" /><label for="description' + count + '"></label></div>')
 	})
 	
-document.querySelector('.acf-table-add-row')?.addEventListener( 'click', function(){
-		$new_leftbody = document.querySelector(this).closest('.acf-table-body-row').next('.acf-table-body-row').querySelector('.acf-table-body-left');
-		$new_count = document.querySelector(this).closest('.acf-table-body-row').next('.acf-table-body-row').querySelector('.acf-table-body-cont').html();
-	document.querySelector( $new_leftbody ).insertAdjacentHTML("beforeend", '<div class="is-description"><input type="checkbox" value="description" id="description' + $new_count + '" name="description" /><label for="description' + $new_count + '"></label></div>' );
-	console.log($new_count);
+	document.querySelectorAll('acf-table-add-row'.forEach(el => el.addEventListener('click', e => {
+		const newLeftBody = e.target.closest('.acf-table-body-row').nextElementSibling.querySelector('.acf-table-body-left');
+		const newCount = e.target.closest('.acf-table-body-row').nextElementSibling.querySelector('.acf-table-body-cont').innerHTML;
+		newLeftBody.appendChild('<div class="is-description"><input type="checkbox" value="description" id="description' + newCount + '" name="description" /><label for="description' + newCount + '"></label></div>')
+	}))
+	
 });
-})
 
 // jQuery.noConflict();
 // (function($) {
--- js/row_classes.js
+++ js/row_classes.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
-	document.querySelector('td.serv-desc')?.closest('tr').classList.add('serv-description');
-	document.querySelector('tr.serv-description')?.closest('tr').previousElementSibling.classList.add('has-description');
+	document.querySelectorAll('td.serv-desc')?.forEach(el => el.closest('tr').classList.add('serv-description') );
+	document.querySelectorAll('tr.serv-description')?.forEach(el => el.closest('tr').previousElementSibling.classList.add('has-description') );
 });
 
 // jQuery.noConflict();
